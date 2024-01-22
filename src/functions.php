966b3cb4e4dfa9436026f94670e6f20662b237ef
Let m3 t0 <78282359+parfaittolefo@users.noreply.github.com>
2024-01-22 13:38:14 +0100

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,8 @@ function get_questions() {
     return [
         ['question' => 'Quelle est la capitale du Bénin?', 'options' => ['Cotonou', 'Porto-Novo', 'Parakou'], 'correct_answer' => 'Porto-Novo'],
         ['question' => 'En quelle année le Bénin a-t-il obtenu son indépendance?', 'options' => ['1959', '1960', '1970'], 'correct_answer' => '1960'],
-        ['question' => 'Quelle est la religion propiétaire du Bénin ', 'options' => ['Fâ', 'Vodoun', 'Quatolique'], 'correct_answer' => 'Vodoun'],
+        ['question' => 'Quelle est la religion propiétaire du Bénin ', 'options' => ['Fâ', 'Vodoun', 'Quatholique'], 'correct_answer' => 'Vodoun'],
+        ['question' => 'Qui était le président du Bénin en 2022', 'options' => ['Dr YAYI BONI', 'Abdoulaye Bio TCHADE', 'Patrice TALON'], 'correct_answer' => 'Patrice TALON'],
         // Ajouter d'autres questions...
     ];
 }
