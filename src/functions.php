438de9109330d016149d12e9ebe98099a44d506c
Let m3 t0 <78282359+parfaittolefo@users.noreply.github.com>
2024-01-22 13:32:40 +0100

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@ function get_questions() {
     return [
         ['question' => 'Quelle est la capitale du Bénin?', 'options' => ['Cotonou', 'Porto-Novo', 'Parakou'], 'correct_answer' => 'Porto-Novo'],
         ['question' => 'En quelle année le Bénin a-t-il obtenu son indépendance?', 'options' => ['1959', '1960', '1970'], 'correct_answer' => '1960'],
-        ['question' => 'Quelle est la religion proiétaire du Bénin ', 'options' => ['Fâ', 'Vodoun', 'Quatolique'], 'correct_answer' => 'Vodoun'],
+        ['question' => 'Quelle est la religion propiétaire du Bénin ', 'options' => ['Fâ', 'Vodoun', 'Quatolique'], 'correct_answer' => 'Vodoun'],
         // Ajouter d'autres questions...
     ];
 }
